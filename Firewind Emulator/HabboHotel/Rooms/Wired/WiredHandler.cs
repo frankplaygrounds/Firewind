df6b5520916b1e673bb6ae6f89604bcefc1361a2
frank <mancini_fra@icloud.com>
2025-01-16 05:02:33 -0800

    oops

--- Firewind Emulator/HabboHotel/Rooms/Wired/WiredHandler.cs
+++ Firewind Emulator/HabboHotel/Rooms/Wired/WiredHandler.cs
@@ -194,10 +194,10 @@ namespace Firewind.HabboHotel.Rooms.Wired
 
             room.SendMessage(message);
 
-            //Task.Run(
-            //    async delegate
-            //    {
-            //        await Task.Delay(0);
+            Task.Run(
+                async delegate
+                {
+                    await Task.Delay(0);
                     message = new ServerMessage(Outgoing.ObjectDataUpdate);
                     message.AppendString(itemID.ToString());
                     message.AppendInt32(0); // datatype
@@ -206,8 +206,8 @@ namespace Firewind.HabboHotel.Rooms.Wired
                     //item.data.AppendToMessage(message);
                     //Console.WriteLine("OnEvent {0}, 0", itemID);
                     room.SendMessage(message);
-            //    }
-            //);
+                }
+            );
         }
         #endregion
 
